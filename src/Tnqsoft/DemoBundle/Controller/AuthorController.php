18a51b681671b7b876c73497af51ac13f671fb20
Nguyen Nhu Tuan <tuanquynh0508@gmail.com>
2017-05-26 00:04:53 +0700

    Implement Crud

--- src/Tnqsoft/DemoBundle/Controller/AuthorController.php
+++ src/Tnqsoft/DemoBundle/Controller/AuthorController.php
@@ -8,6 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 
 use Tnqsoft\DemoBundle\Entity\Author;
+use Tnqsoft\DemoBundle\Form\AuthorType;
 
 /**
  * Author controller.
@@ -17,7 +18,7 @@ use Tnqsoft\DemoBundle\Entity\Author;
 class AuthorController extends Controller
 {
     /**
-     * Lists all author entities.
+     * Lists all entities.
      *
      * @Route("/", name="admin_author_list")
      * @Method("GET")
@@ -40,5 +41,111 @@ class AuthorController extends Controller
             'orderDir' => $orderDir,
             'keyword' => $keyword,
         ));
+    }
+
+    /**
+     * Finds and displays a entity.
+     *
+     * @Route("/{id}", name="admin_author_show")
+     * @Method("GET")
+     */
+    public function showAction(Author $entity)
+    {
+        $deleteForm = $this->createDeleteForm($entity);
+
+        return $this->render('TnqsoftDemoBundle:Author:show.html.twig', array(
+            'entity' => $entity,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Creates a new entity.
+     *
+     * @Route("/new", name="admin_author_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $entity = new Author();
+        $form = $this->createForm(AuthorType::class, $entity);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_author_show', array('id' => $entity->getId()));
+        }
+
+        return $this->render('TnqsoftDemoBundle:Author:new.html.twig', array(
+            'entity' => $entity,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing entity.
+     *
+     * @Route("/{id}/edit", name="admin_author_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Author $entity)
+    {
+        $deleteForm = $this->createDeleteForm($entity);
+        $form = $this->createForm(AuthorType::class, $entity);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $this->getDoctrine()->getManager()->flush();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_author_edit', array('id' => $entity->getId()));
+        }
+
+        return $this->render('TnqsoftDemoBundle:Author:edit.html.twig', array(
+            'entity' => $entity,
+            'form' => $form->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a author entity.
+     *
+     * @Route("/{id}", name="admin_author_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Author $entity)
+    {
+        $form = $this->createDeleteForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('admin_author_list');
+    }
+
+    /**
+     * Creates a form to delete a entity.
+     *
+     * @param Author $entity The author entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Author $entity)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('admin_author_delete', array('id' => $entity->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
     }
 }
---
+++ src/Tnqsoft/MaterialBundle/Twig/Extension/RoutingExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tnqsoft\MaterialBundle\Twig\Extension;
+
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Bridge\Twig\Extension\RoutingExtension as BaseRoutingExtension;
+
+class RoutingExtension extends BaseRoutingExtension
+{
+    /**
+     * @var RequestStack
+     */
+    private $requestStack;
+
+    public function __construct(Router $generator, RequestStack $requestStack)
+    {
+        parent::__construct($generator);
+        $this->requestStack = $requestStack;
+    }
+
+    /**
+     * @param string $name
+     * @param array  $parameters
+     * @param bool   $relative
+     * @param bool   $keepParams
+     *
+     * @return string
+     */
+    public function getPath($name, $parameters = array(), $relative = false, $keepParams = false)
+    {
+        $request = $this->requestStack->getCurrentRequest();
+        if ($keepParams === true) {
+            $parameters = array_merge($parameters, $request->query->all());
+        }
+        return parent::getPath($name, $parameters, $relative);
+    }
+}
